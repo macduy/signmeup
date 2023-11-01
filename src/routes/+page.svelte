b64416a7874b6be598ceb77e67222746af541ed5
Hai <macduy@gmail.com>
2023-11-01 23:21:08 +0000

    Input field

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
+<script>
+	import Echo from "../Echo.svelte";
+
+</script>
+
+
 <h1>Sign Me Up</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<Echo />
