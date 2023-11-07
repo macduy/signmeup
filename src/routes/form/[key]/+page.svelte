f490a3a2dd369965a485a67459ddf0c08dbf442a
Hai <macduy@gmail.com>
2023-11-07 07:10:35 +0000

    Main form updates

--- src/routes/form/[key]/+page.svelte
+++ src/routes/form/[key]/+page.svelte
@@ -5,11 +5,26 @@
 </script>
 
 <div class="container p-3">
-	<h1>{data.form?.key}</h1>
-
 	<form method="POST">
-		Email:
-		<input type="email" name="email" />
-		<button>Submit</button>
+		<div class="form-control w-full max-w-xs">
+			<label class="label" for="name">
+				<span class="label-text">Your name:</span>
+			</label>
+			<input type="text" name="name" placeholder="Your name" class="input input-bordered w-full" />
+		</div>
+
+		<div class="form-control w-full max-w-xs">
+			<label class="label" for="email">
+				<span class="label-text">Your email address:</span>
+			</label>
+			<input
+				type="email"
+				name="email"
+				placeholder="Your email"
+				class="input input-bordered w-full"
+			/>
+		</div>
+
+		<button class="btn btn-accent mt-4">Submit</button>
 	</form>
 </div>
